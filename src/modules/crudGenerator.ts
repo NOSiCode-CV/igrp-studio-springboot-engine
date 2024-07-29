fb7e69cd9b8cdcd1e400e4d62f4134d1ab24a970
Eduardo Fernando <eduardo.fernando@wayvant.io>
2024-07-29 16:04:37 +0200

    crud refactor

--- src/modules/crudGenerator.ts
+++ src/modules/crudGenerator.ts
@@ -4,14 +4,7 @@ import { repository } from './repositoryGenerator';
 
 export const crudGenerator = async (config: ModelConfig, output: string) => {
 
-    //  Another solution is to consider that this method will be called only in case the model has no crud or you want to update the existing crud.
-    // In this case, the configuration of the existing model will always be updated.
+  await saveModelFiles(config, output);
 
-    //steps:
-
-    //1- update the existing modelConfig
-    await saveModelFiles(config, output);
-
-    //2- call the repository generator
-    await repository(config, output);
+  await repository(config, output);
 };
