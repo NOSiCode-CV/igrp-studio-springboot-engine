195c39a51c847484317c5c6160a4d724bf48cec6
Eduardo Fernando <eduardo.fernando@wayvant.io>
2024-10-02 16:38:54 +0200

    constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,8 +2,8 @@ import path from 'path';
 import { TypeMetadata } from '../interfaces/types';
 import { getDTOTypes } from '../modules/dto/helpers';
 
-export const TEMPLATE_DIR = path.join(__dirname, '../../public/templates');
-// export const TEMPLATE_DIR = path.join(__dirname, './templates');
+// export const TEMPLATE_DIR = path.join(__dirname, '../../public/templates');
+export const TEMPLATE_DIR = path.join(__dirname, './templates');
 
 export const DIRECTORIES = {
   BASE_API: '.igrpstudio/baseApi.json',
