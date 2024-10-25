e89e7477603f78d8cbf9eefa4189580f21526a36
Eduardo Fernando <eduardo.fernando@wayvant.io>
2024-10-25 11:28:22 +0200

    removed function to verify duplicateds in ajv-instances

--- src/utils/ajv-instance.ts
+++ src/utils/ajv-instance.ts
@@ -6,17 +6,4 @@ const ajvInstance = new Ajv({ allErrors: true, allowUnionTypes: true });
 addFormats(ajvInstance);
 addErrors(ajvInstance);
 
-const checkUniqueActionNames = (arr: any) => {
-  const names = arr.map((action: { actionName: string; }) => action.actionName);
-  const hasDuplicates = names.some((name: string, index: number) => names.indexOf(name) !== index);
-  return !hasDuplicates;
-}
-
-ajvInstance.addKeyword({
-  keyword: 'uniqueActionNames',
-  type: 'array',
-  validate: checkUniqueActionNames,
-});
-
-
 export { ajvInstance };
