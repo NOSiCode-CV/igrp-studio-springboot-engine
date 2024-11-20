70387a30a103281a3bf17054226ad2b6a1a82336
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2024-11-20 18:36:28 -0100

    handle empty attributes in commands and queries

--- src/utils/handlebarsHelpers.ts
+++ src/utils/handlebarsHelpers.ts
@@ -325,4 +325,9 @@ Handlebars.registerHelper('filterQueryActions', function(array: ControllerAction
     .join('');
 });
 
+Handlebars.registerHelper('and', function (...args) {
+  const options = args.pop(); // Remove the last element, which is the Handlebars options object
+  return args.every(Boolean); // Check if all arguments are truthy
+});
+
 export { Handlebars };
