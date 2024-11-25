c2c1fb0d5b8d2c924886cc5e9ded9e6b48c5efbe
william.fonseca <william.fonseca@nosi.cv>
2024-11-25 21:40:57 +0000

    remove request body validations for some methods

--- src/modules/controller/generateController.ts
+++ src/modules/controller/generateController.ts
@@ -26,7 +26,6 @@ export const generateController = async (context: RenderContext<ControllerConfig
   const controller = await renderController(context);
   const allTypes = await getDtos(context)
 
-  checkAcceptsAndRequestBody(context.resourceConfig.actions)
   await verifyResponseAndRequestBodyTypes(context.resourceConfig.actions, allTypes)
   await checkPermission(context.resourceConfig, context.basePath)
   
@@ -83,15 +82,3 @@ const verifyResponseAndRequestBodyTypes = async (actions: ControllerAction[], ty
       }
   }*/
 }
-
-const checkAcceptsAndRequestBody = (actions: ControllerAction[]) => {
-  const method = ['POST', 'PUT', 'PATCH']
-  actions.map(action => {
-    if (method.includes(action.method) && !action.requestBody)
-      throw ERROR_MESSAGE.REQUEST_BODY_REQUIRED
-    
-    if (method.includes(action.method) && !action.accepts)
-      throw ERROR_MESSAGE.ACCEPTS_REQUIRED
-  })
-}
-
