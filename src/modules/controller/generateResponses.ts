cf7a5c02f848cbab11ebe1407ef8e3115326b19a
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2025-02-07 15:21:45 -0100

    fix no response generation condition [v.0.3.0-alpha-5.0.2]

--- src/modules/controller/generateResponses.ts
+++ src/modules/controller/generateResponses.ts
@@ -39,8 +39,8 @@ export const generateResponses = async (context: RenderContext<ControllerConfig>
 
     for (const [status, response] of Object.entries(action.responses)) {
 
-      // If there's no name attribute that means it's a reference, so do not need to generate the DTO
-      if(!response.name && (response?.content['application/json'] ?? response?.content['multipart/form-data'])) continue;
+      // If it's a reference attribute that means do not need to generate the DTO
+      if((response?.content['application/json'] ?? response?.content['multipart/form-data'])?.schema.objectType) continue;
       if(status == "204") continue;
 
       // Capitalize the response name
