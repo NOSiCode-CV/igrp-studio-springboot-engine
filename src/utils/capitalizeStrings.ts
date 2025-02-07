17221788d5319460be6c001bbfa37916f7db72f8
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2025-02-07 14:39:12 -0100

    fix error in null pointer in capitalizeString.capitalizeResponse method [v.0.3.0-alpha-5.0.1]

--- src/utils/capitalizeStrings.ts
+++ src/utils/capitalizeStrings.ts
@@ -14,5 +14,5 @@ export const capitalizeResponse = (responses?: { [p: string]: Body }): string =>
   const singleBody = responses[keys[0]];
 
   // Return the "name" attribute if it exists, otherwise return an empty string
-  return singleBody?.content[0].schema.objectType? capitalize(singleBody?.content[0].schema.type?.replace(/dto$/i, '') + "DTO") : capitalize(singleBody?.name.replace(/dto$/i, '') + "DTO")  || "?";
+  return singleBody?.content[0]?.schema.objectType? capitalize(singleBody?.content[0].schema.type?.replace(/dto$/i, '') + "DTO") : capitalize(singleBody?.name.replace(/dto$/i, '') + "DTO")  || "?";
 };
