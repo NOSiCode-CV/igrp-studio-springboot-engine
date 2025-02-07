429c10000be94abf4be2a96131b0e7ae12d61340
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2025-02-07 15:23:06 -0100

    add null check in reference type validation [v.0.3.0-alpha-5.0.2]

--- src/utils/capitalizeStrings.ts
+++ src/utils/capitalizeStrings.ts
@@ -14,5 +14,5 @@ export const capitalizeResponse = (responses?: { [p: string]: Body }): string =>
   const singleBody = responses[keys[0]];
 
   // Return the "name" attribute if it exists, otherwise return the type from the reference
-  return ((singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data']).schema.objectType)? capitalize((singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data']).schema.type?.replace(/dto$/i, '') + "DTO") : capitalize(singleBody?.name.replace(/dto$/i, '') + "DTO")  || "?";
+  return ((singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data'])?.schema.objectType)? capitalize((singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data'])?.schema.type?.replace(/dto$/i, '') + "DTO") : capitalize(singleBody?.name.replace(/dto$/i, '') + "DTO")  || "?";
 };
