f4199a87b69f16bbe1449a12eb79812ea0393810
william.fonseca <william.fonseca@nosi.cv>
2025-03-10 17:10:59 -0100

    Refactor capitalizeResponse function logic.
    Simplified the capitalizeResponse function by restructuring its logic, improving readability and maintainability. Introduced helper methods to handle type processing and response collections more effectively. Fixed edge cases for schema type handling and ensured compliance with primitive type outputs.

--- src/utils/capitalizeStrings.ts
+++ src/utils/capitalizeStrings.ts
@@ -1,5 +1,6 @@
 import { Body } from '../interfaces/types';
 import { isResponseCollection } from './helpers';
+import { type } from 'node:os';
 
 const singleCapitalize = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1);
 
@@ -17,38 +18,44 @@ export const processJavaClassName = (str: string): string =>
     .join('');
 
 export const capitalizeResponse = (responses?: { [p: string]: Body }): string => {
-  if (!responses) return '?'; // Return empty string if responses is undefined
 
-  const keys = Object.keys(responses);
+  if (!responses || Object.keys(responses).length !== 1) return '?';
 
-  if (keys.length !== 1) return '?'; // Return "?" if there is more than one key
+  const singleBody = responses[Object.keys(responses)[0]];
+  const content = singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data'];
+  const schema = content?.schema;
 
-  const singleBody = responses[keys[0]];
+  if (!schema) return '?';
 
-  // Return the "name" attribute if it exists, otherwise return the type from the reference
-  const response = (
-    singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data']
-  )?.schema.objectType
-    ? capitalize(
-        (
-          singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data']
-        )?.schema.type?.replace(/dto$/i, '') + 'DTO',
-      )
-    : isResponseCollection(
-          (singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data'])
-            ?.schema.type,
-        )
-      ? capitalize(
-          (
-            singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data']
-          )?.schema.items?.type?.replace(/dto$/i, '') + 'DTO',
-        )
-      : capitalize(singleBody?.name?.replace(/dto$/i, '') + 'DTO') || '?';
+  const getTypeName = (type: string | undefined): string =>
+    capitalize(type?.replace(/dto$/i, '') + 'DTO');
 
-  return isResponseCollection(
-    (singleBody?.content['application/json'] ?? singleBody?.content['multipart/form-data'])?.schema
-      .type,
-  )
-    ? `List<${response}>`
-    : response;
+  const processResponseCollection = (type: string): string => {
+    switch (type) {
+      case 'collection':
+        return `Collection<${type}>`
+      case 'map':
+        return `Map<?, ${type}>`
+      case 'pageable':
+        return `Page<${type}>`
+    }
+    return type;
+  }
+
+  let response: string;
+
+  if (schema.type === 'object') {
+    response = getTypeName(singleBody?.name);
+  } else {
+
+    let primitiveTypes: string[] = ["integer", "boolean", "string"];
+
+    if (primitiveTypes.includes(schema.type)) {
+      response = capitalize(schema.type);
+    } else {
+      response = getTypeName(schema.type);
+    }
+  }
+
+  return processResponseCollection(response);
 };
