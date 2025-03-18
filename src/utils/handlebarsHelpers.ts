725a60766aba362eecaf552dd5c291e17402128d
william.fonseca <william.fonseca@nosi.cv>
2025-03-18 15:55:47 -0100

    helper functions refactoring

--- src/utils/handlebarsHelpers.ts
+++ src/utils/handlebarsHelpers.ts
@@ -1,14 +1,11 @@
 import * as Handlebars from 'handlebars';
 
 import {
-  ApiConfig,
   Attribute,
   Body,
   ControllerAction,
-  DTOConfig,
   HttpHeader,
   JavaAttribute,
-  JavaType,
   ModelConfig,
   Relation,
   SchemaContent,
@@ -17,7 +14,6 @@ import {
   DIRECTORIES,
   GENERIC_IMPORTS,
   GENERIC_TYPES,
-  PACKAGES,
   PROJECT_STRUCTURE_STYLE,
   REQUEST_BODY_NOT_IMPORT,
   REQUEST_MAPPING_OPTIONS,
@@ -26,7 +22,6 @@ import {
   getPackageNameFromConfig,
   isPageable,
   isResponseCollection,
-  validateAnnotations,
 } from './helpers';
 import { capitalizeResponse, wrapCollectionType } from './capitalizeStrings';
 import { normalizeName } from '../modules/dto/saveDTOConfig';
@@ -40,8 +35,9 @@ import {
 } from '../helper/stringHelper';
 import { json } from '../helper/jsonHelper';
 import { ifEquals, ifNot, not } from '../helper/logicalHelper';
-import { keyTypeModel } from '../helper/modelHelper';
+import { keyTypeModel, modelImport } from '../helper/modelHelper';
 import { keyTypeDTO } from '../helper/dtoHelper';
+import { resolveAnnotations, resolvePackage } from '../helper/generalHelper';
 
 // String
 Handlebars.registerHelper('capitalize',capitalize);
@@ -67,9 +63,14 @@ Handlebars.registerHelper('ifEquals',ifEquals);
 
 // MODEL
 Handlebars.registerHelper('keyType',keyTypeModel);
+Handlebars.registerHelper('model-imports-helper',modelImport);
 
 // DTO
 Handlebars.registerHelper('keyType', keyTypeDTO);
+
+// GENERAL
+Handlebars.registerHelper('resolve-package',resolvePackage);
+Handlebars.registerHelper('resolve-annotations', resolveAnnotations);
 
 Handlebars.registerHelper('resolveResponse', function (responses?: { [p: string]: Body }): string {
   return capitalizeResponse(responses);
@@ -183,128 +184,6 @@ Handlebars.registerHelper(
     return [...new Set(imports)].join('\n');
   },
 );
-
-Handlebars.registerHelper('resolve-annotations', function (attribute) {
-  const annotations = [];
-
-  validateAnnotations(attribute);
-
-  // JSON / XML annotations
-  if (attribute.jsonAttributeName) {
-    annotations.push(`@JsonProperty("${attribute.jsonAttributeName}")`);
-  }
-
-  if (attribute.xmlAttributeName) {
-    annotations.push(`@JacksonXmlProperty(localName = "${attribute.jsonAttributeName}")`);
-  }
-
-  // Required validation
-  if (attribute.required) {
-    if (attribute.type === 'string') {
-      annotations.push(`@NotBlank(message = "The field <${attribute.name}> is required.")`);
-    } else {
-      annotations.push(`@NotNull(message = "The field <${attribute.name}> is required.")`);
-    }
-  }
-
-  // String-specific validations
-  if (attribute.type === 'string') {
-    if (attribute.minLength !== undefined)
-      annotations.push(
-        `@Size(${[
-          `min = ${attribute.minLength}, message = "The field length <${attribute.name}> must be at least ${attribute.minLength} characters."`,
-        ]
-          .filter(Boolean)
-          .join(', ')})`,
-      );
-    if (attribute.maxLength !== undefined)
-      annotations.push(
-        `@Size(${[
-          `max = ${attribute.maxLength}, message = "The field length <${attribute.name}> cannot be more than ${attribute.maxLength} characters."`,
-        ]
-          .filter(Boolean)
-          .join(', ')})`,
-      );
-    if (attribute.regex) {
-      annotations.push(
-        `@Pattern(message = "Invalid value format for field <${attribute.name}>.", regexp = "${attribute.regex.replace(/\\/g, '\\\\')}")`,
-      );
-    }
-  }
-
-  // Number-specific validations
-  if (['Integer', 'Long', 'Double', 'Float', 'BigDecimal', 'BigInteger'].includes(attribute.type)) {
-    if (attribute.minLength !== undefined) {
-      annotations.push(`@Min(${attribute.minLength})`);
-    }
-    if (attribute.maxLength !== undefined) {
-      annotations.push(`@Max(${attribute.maxLength})`);
-    }
-    if (attribute.positive) {
-      annotations.push(
-        attribute.minLength === 0
-          ? `@PositiveOrZero(message = "<${attribute.name}> must be greater than or equal to zero.")`
-          : `@Positive(message = "<${attribute.name}> must be greater than zero.")`,
-      );
-    }
-  }
-
-  // Date-specific validations
-  if (['Date', 'LocalDate', 'LocalDateTime', 'ZonedDateTime'].includes(attribute.type)) {
-    if (attribute.before) {
-      annotations.push(
-        `@Past(message = "The date <${attribute.name}> must be before today's date.")`,
-      );
-    }
-    if (attribute.after) {
-      annotations.push(
-        `@Future(message = "The date <${attribute.name}> must be after today's date.")`,
-      );
-    }
-  }
-
-  // Email validation
-  if (attribute.isEmail) {
-    annotations.push(`@Email(message = "Invalid email format for field <${attribute.name}>.")`);
-  }
-
-  // URL validation
-  if (attribute.isUrl) {
-    annotations.push(`@URL(message = "Invalid URL format for field <${attribute.name}>.")`);
-  }
-
-  // Return the generated annotations as a joined string
-  return annotations.join('\n\t');
-});
-
-Handlebars.registerHelper('resolve-package', function (fullPath, basePath) {
-  if (!fullPath || !basePath) {
-    throw new Error('Both fullPath and basePath are required to resolve the package.');
-  }
-
-  // Normalize paths to handle both Unix and Windows formats
-  const normalizedFullPath = fullPath.replace(/\\/g, '/');
-  const normalizedBasePath = basePath.replace(/\\/g, '/');
-
-  // Ensure the basePath ends with a trailing slash for accurate replacement
-  const formattedBasePath = normalizedBasePath.endsWith('/')
-    ? normalizedBasePath
-    : normalizedBasePath + '/';
-
-  // Remove the basePath portion from the full path
-  let relativePath: string;
-  if (normalizedFullPath.startsWith(formattedBasePath)) {
-    relativePath = normalizedFullPath.slice(formattedBasePath.length);
-  } else {
-    return '';
-  }
-
-  // Extract only the meaningful parts of the path for the Java package
-  return relativePath
-    .split('/')
-    .filter((segment) => !['src', 'main', 'test', 'java'].includes(segment)) // Exclude common directory names
-    .join('.');
-});
 
 Handlebars.registerHelper(
   'eq',
@@ -660,103 +539,5 @@ Handlebars.registerHelper('addPropPageable', function (context): boolean {
 
   return isPageable;
 });
-
-Handlebars.registerHelper(
-  'model-imports-helper',
-  function (modelConfig: ModelConfig, baseConfig: ApiConfig) {
-    const imports: string[] = [];
-    imports.push(`import cv.igrp.framework.stereotype.IgrpEntity;`);
-    imports.push(`import jakarta.persistence.*;`);
-    imports.push(`import lombok.*;`);
-
-    const packageNameFromConfig = getPackageNameFromConfig(baseConfig);
-
-    const isDDDStyle =
-      baseConfig.projectStructureStyle === PROJECT_STRUCTURE_STYLE.DOMAIN_DRIVEN_DESIGN;
-
-    if (modelConfig.revision === true) imports.push(`import org.hibernate.envers.Audited;`);
-
-    modelConfig.attributes.forEach((attribute) => {
-      const type = GENERIC_TYPES.get(attribute.type)?.java.name;
-
-      if (
-        type === 'LocalTime' ||
-        type === 'LocalDate' ||
-        type === 'LocalDateTime' ||
-        type === 'ZoneDateTime' ||
-        type === 'OffsetDateTime' ||
-        type === 'Instant'
-      ) {
-        imports.push(`import java.time.${type};`);
-      }
-
-      if (type === 'BigInteger' || type === 'BigDecimal') imports.push(`import java.math.${type};`);
-
-      if (attribute.nullable === false && !attribute.primaryKey) {
-        if (type === 'String') imports.push('import jakarta.validation.constraints.NotBlank;');
-        else imports.push('import jakarta.validation.constraints.NotNull;');
-      }
-
-      if (type === 'UUID') imports.push('import java.util.UUID;');
-
-      if (attribute.skipFieldRevision) imports.push('import org.hibernate.envers.NotAudited;');
-
-      if (attribute.defaultValue) imports.push('import org.hibernate.annotations.ColumnDefault;');
-
-      if (attribute.relation?.type === 'OneToMany' || attribute.relation?.type === 'ManyToMany')
-        imports.push('import java.util.List;');
-
-      if (attribute.relation?.entity) {
-        if (isDDDStyle) {
-          if (modelConfig.module !== attribute.relation.module)
-            imports.push(
-              `import ${packageNameFromConfig}.${attribute.relation.module ?? DIRECTORIES.SHARED}.domain.${PACKAGES.MODELS}.${capitalize(attribute.relation.entity)};`,
-            );
-        } else
-          imports.push(
-            `import ${packageNameFromConfig}.${PACKAGES.MODELS}.${attribute.relation.entity.toLowerCase()}.${capitalize(attribute.relation.entity)};`,
-          );
-      }
-
-      if (attribute.objectType) {
-        const objectImports = GENERIC_IMPORTS(
-          packageNameFromConfig,
-          attribute.type,
-          attribute.module,
-        ).get(attribute.objectType);
-
-        if (objectImports) {
-          imports.push(
-            (isDDDStyle ? objectImports.java.domain : objectImports.java.technical) ?? '',
-          );
-          return;
-        }
-      }
-
-      const specialAttributeImports = GENERIC_IMPORTS(packageNameFromConfig, attribute.type).get(
-        attribute.type,
-      );
-
-      if (specialAttributeImports?.java.technical)
-        imports.push(specialAttributeImports.java.technical);
-    });
-
-    modelConfig.relationReference?.forEach((rel) => {
-      if (rel.type === 'ManyToMany' || rel.type === 'OneToMany' || rel.type === 'ManyToOne')
-        imports.push('import java.util.List;');
-      if (isDDDStyle) {
-        if (modelConfig.module !== rel.module)
-          imports.push(
-            `import ${packageNameFromConfig}.${rel.module ?? DIRECTORIES.SHARED}.domain.${PACKAGES.MODELS}.${capitalize(rel.entity)};`,
-          );
-      } else
-        imports.push(
-          `import ${packageNameFromConfig}.${PACKAGES.MODELS}.${rel.entity.toLowerCase()}.${capitalize(rel.entity)};`,
-        );
-    });
-
-    return [...new Set(imports)].join('\n');
-  },
-);
 
 export { Handlebars };
