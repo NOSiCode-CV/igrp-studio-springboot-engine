37db4e00ed2d2cd33da8db40fb171945fe5d4e30
william.fonseca <william.fonseca@nosi.cv>
2025-03-24 18:16:00 -0100

    add H2 database option

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -919,7 +919,7 @@ export const SCHEMA_TYPES = [
   'Reference other Object',
 ] as const;
 export const REQUEST_BODY_NOT_IMPORT = ['String', 'Integer', 'Boolean', 'Object'];
-export const DATABASE_TYPES = ['MySQL', 'Oracle', 'Postgresql'] as const;
+export const DATABASE_TYPES = ['MySQL', 'Oracle', 'Postgresql', 'H2'] as const;
 export const STRUCT_TYPES = [
   PROJECT_STRUCTURE_STYLE.DOMAIN_DRIVEN_DESIGN,
   PROJECT_STRUCTURE_STYLE.TECHNICAL,
